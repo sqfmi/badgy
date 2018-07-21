7cdb1da8255d5d554cf50971d8fb3c7aa82c8902
SQFMI <github@sqfmi.com>
2018-07-21 19:10:28 -0400

    Update badgy.ino

--- firmware/badgy.ino
+++ firmware/badgy.ino
@@ -2,38 +2,65 @@
 #include <GxGDEH029A1/GxGDEH029A1.cpp>
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
+#include <Fonts/FreeMonoBold9pt7b.h> //include any other fonts you want to use https://github.com/adafruit/Adafruit-GFX-Library
 #include <Fonts/FreeSansBoldOblique24pt7b.h>
 #include "hello.h"
 
-GxIO_Class io(SPI, SS, D3, D4);
-GxEPD_Class display(io);
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <WiFiManager.h>
 
+/* Always include the update server, or else you won't be able to do OTA updates!*/
+/**/const int port = 8888;
+/**/ESP8266WebServer httpServer(port);
+/**/ESP8266HTTPUpdateServer httpUpdater;
+/*********************************************************************************/
+
+/* Configure pins for display */
+GxIO_Class io(SPI, SS, 0, 2);
+GxEPD_Class display(io); // default selection of D4, D2
+
+/* A single byte is used to store the button states for debouncing */
 byte buttonState = 0;
-byte lastButtonState = 0;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
+byte lastButtonState = 0;   //the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  //the last time the output pin was toggled
+unsigned long debounceDelay = 50;    //the debounce time
 
 void setup()
 {  
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("Booting Sketch...");
-
   display.init();
-  pinMode(1,INPUT_PULLUP);  //down
-  pinMode(3,INPUT_PULLUP);  //up
-  pinMode(5,INPUT_PULLUP);  //center
+  showWelcome(); //show welcome message immediately on boot
+  
+  pinMode(1,INPUT_PULLUP); //down
+  pinMode(3,INPUT_PULLUP); //left
+  pinMode(5,INPUT_PULLUP); //center
   pinMode(12,INPUT_PULLUP); //right
-  pinMode(16,INPUT);        //left GPIO16 has an external pull-up resistor
+  pinMode(10,INPUT_PULLUP); //up
+
+  /* WiFi Manager automatically connects using the saved credentials, if that fails it will go into AP mode */
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Badgy AP");
+
+  /* Once connected to WiFi, startup the OTA update server*/
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+
+  /* Show IP Address on display*/
+  showIP();
 }
 
 void loop()
 {
-  byte reading =  (digitalRead(1)  == 0 ? 0 : (1<<0)) |
-                  (digitalRead(3)  == 0 ? 0 : (1<<1)) |
-                  (digitalRead(5)  == 0 ? 0 : (1<<2)) |
-                  (digitalRead(12) == 0 ? 0 : (1<<3)) |
-                  (digitalRead(16) == 0 ? 0 : (1<<4));
+  httpServer.handleClient();
+  
+  byte reading =  (digitalRead(1)  == 0 ? 0 : (1<<0)) | //down
+                  (digitalRead(3)  == 0 ? 0 : (1<<1)) | //left
+                  (digitalRead(5)  == 0 ? 0 : (1<<2)) | //center
+                  (digitalRead(12) == 0 ? 0 : (1<<3)) | //right
+                  (digitalRead(10) == 0 ? 0 : (1<<4));  //up
                   
   if(reading != lastButtonState){
     lastDebounceTime = millis();
@@ -45,19 +72,19 @@ void loop()
         if(bitRead(buttonState, i) == 0){
           switch(i){
             case 0:
-              //do something on down
+              showText("You Pressed Down!");
               break;
             case 1:
-              //do something on up
+              showText("You Pressed Left!");
               break;
             case 2:
-              showBadge(); //do something on center
+              showText("You Pressed Center!");
               break;
             case 3:
-              //do something on right
+              showText("You Pressed Right!");
               break;
             case 4:
-              //do something on left
+              showText("You Pressed Up!");
               break;                              
             default:
               break;
@@ -69,12 +96,46 @@ void loop()
   lastButtonState = reading;
 }
 
-void showBadge()
+void showText(char *text)
 {
-  display.setRotation(3);
+  display.setRotation(3); //even = portrait, odd = landscape
+  display.fillScreen(GxEPD_WHITE);
+  const GFXfont* f = &FreeMonoBold9pt7b ;
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(10,70);
+  display.println(text);
+  display.update();
+}
+
+void showIP(){
+  display.setRotation(3); //even = portrait, odd = landscape
+  display.fillScreen(GxEPD_WHITE);
+  const GFXfont* f = &FreeMonoBold9pt7b ;
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(0,10);
+
+  String ip = WiFi.localIP().toString();
+  String url = WiFi.localIP().toString() + ":"+String(port)+"/update";
+  byte charArraySize = url.length() + 1;
+  char urlCharArray[charArraySize];
+  url.toCharArray(urlCharArray, charArraySize);
+
+  display.println("You are now connected!");
+  display.println("");  
+  display.println("Go to:");
+  display.println(urlCharArray);
+  display.println("to upload a new sketch.");
+  display.update();  
+}
+
+void showWelcome()
+{
+  display.setRotation(3); //even = portrait, odd = landscape
   display.fillScreen(GxEPD_WHITE);
   display.drawBitmap(hello, 0, 0, 296, 128, GxEPD_WHITE);
-  const char* name = "badgy"; //name on badge
+  const char* name = "badgy";
   const GFXfont* f = &FreeSansBoldOblique24pt7b;
   display.setTextColor(GxEPD_BLACK);
   display.setFont(f);
